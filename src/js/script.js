c4a12480de266760153529e53d5b7d85713076a6
Giacomo Cerquone <cerquone@gmail.com>
2016-06-08 20:31:37 +0200

    Added linter, completely removed jquery, avoided namespace pollution, smarter coding wit npm modules, true js minified file, working to implement less

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-66906230-1', 'auto');
+ga('send', 'pageview');
+
+(function() {
+  var burgerMenu  = document.getElementById('burger'),
+      header      = document.getElementById('header'),
+      main        = document.getElementById('main'),
+      burgerIcon  = document.getElementById('burger-icon');
+
+  burgerMenu.addEventListener('click', function() {
+    if(burgerIcon.classList.contains('fa-bars')) {
+      header.style.right = '0';
+      main.style.transform = 'translateX(-16em)';
+      burgerIcon.classList.add('fa-times');
+      burgerIcon.classList.remove('fa-bars');
+      burgerMenu.style.right = '-.2em';
+    } else {
+      header.style.right = '-16em';
+      main.style.transform = 'translateX(0em)';
+      burgerIcon.classList.remove('fa-times');
+      burgerIcon.classList.add('fa-bars');
+    }
+
+  });
+})();
